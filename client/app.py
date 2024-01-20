2a09331d37ffb75daf231f77888832bb8f61e6e4
Paul <72017436+paul-baiju@users.noreply.github.com>
2024-01-20 13:28:30 +0530

    paul

--- client/app.py
+++ client/app.py
@@ -1,10 +1,11 @@
 # app.py
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, jsonify
 import RPi.GPIO as GPIO
 import threading
 import time
 import os
 import webbrowser
+from mfrc522 import SimpleMFRC522
 
 app = Flask(__name__)
 
@@ -14,13 +15,24 @@ motor_pin_1 = 11
 motor_pin_2 = 37
 motor_pin_3 = 36
 tsop_pin = 32
+rfid_power_pin = 22
 
 GPIO.setwarnings(False)
 GPIO.setup(motor_pin_1, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(motor_pin_2, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(motor_pin_3, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(rfid_power_pin, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(tsop_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+reader = SimpleMFRC522()
+
+def enable_rfid_module():
+    GPIO.output(rfid_power_pin, GPIO.HIGH)
+
+def disable_rfid_module():
+    GPIO.output(rfid_power_pin, GPIO.LOW)
+    
+    
 # Variable to track the display state
 display_is_sleeping = False
 display_sleep_offset = 0
@@ -30,14 +42,14 @@ def tsop_sensor_thread():
 	while True:
 		if GPIO.input(tsop_pin) == GPIO.LOW:
 			if display_is_sleeping:
-				os.system("vcgencmd display_power 1")
+				os.system("xset dpms force on")
 				display_is_sleeping = False
 				display_sleep_offset = 0
 				time.sleep(1)
 		else:
 			if not display_is_sleeping and display_sleep_offset > 5:
-				print("sleep")
-				os.system("vcgencmd display_power 0")
+				os.system("xset dpms force off")
+				disable_rfid_module()
 				display_is_sleeping = True
 			elif display_sleep_offset <= 5:
 				display_sleep_offset = display_sleep_offset + 1
@@ -47,9 +59,27 @@ def tsop_sensor_thread():
 tsop_thread = threading.Thread(target=tsop_sensor_thread)
 tsop_thread.start()
 
+def read_rfid():
+	try:
+		# Read the RFID tag
+		id, rfid_text = reader.read()
+		return id, rfid_text
+	except Exception as e:
+		print(f"Error reading RFID: {e}")
+		return None, None
+	
 @app.route("/")
 def index():
     return render_template('index.html')
+    
+@app.route("/rfid")
+def rfid():
+    return render_template('rfid.html')
+    
+@app.route("/pay")
+def pay():
+    return render_template('pay.html')
+
 
 @app.route("/css/<path:filename>")
 def serve_css(filename):
@@ -61,6 +91,29 @@ def serve_js(filename):
     js_dir = os.path.join(app.root_path, 'js')
     return send_from_directory(js_dir, filename)
     
+@app.route("/scanRFID")
+def scan_RFID():
+	try:
+		print("Waiting for RFID scan...")
+		id, rfid_text = None, None
+		# Enable RFID module before scanning
+		enable_rfid_module()
+
+		while id is None:
+			id, rfid_text = read_rfid()
+			time.sleep(0.1)
+		print(id, rfid_text)
+
+		# Disable RFID module after scanning
+		disable_rfid_module()
+		# Return the id and rfid_text as JSON
+		return jsonify(id=id, rfid_text=rfid_text)
+
+	except Exception as e:
+		# Disable RFID module in case of an error
+		disable_rfid_module()
+		return jsonify(error=str(e))
+		
 @app.route("/controlMotor1")
 def control_motor_1():
     # Turn on the motor for 2 seconds as an example
@@ -87,4 +140,4 @@ def control_motor_3():
 
 if __name__ == "__main__":
 	webbrowser.open('http://0.0.0.0:5000', new=1, autoraise=True)
-	app.run(debug=True, host="0.0.0.0", port=5000)
+	app.run(debug=False, host="0.0.0.0", port=5000)
--- client/js/choice.js
+++ client/js/choice.js
@@ -44,11 +44,51 @@ function checkout() {
     element.style.display = 'flex';
 }
 
-function choosePay() {
+function choosePay(type) {
+    if (type === 'rfid') {
+        window.location.href = './rfid?size=' + size + '&qty=' + qty;
+    } else {
+        window.location.href = './pay?size=' + size + '&qty=' + qty;
+    }
+}
+
+function scanRFID() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "/scanRFID", true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            // Parse the JSON response
+            var response = JSON.parse(xhr.responseText);
+
+            // Log the id and rfid_text
+            console.log("ID:", response.id);
+            var size = getUrlParameter("size");
+            var qty = getUrlParameter("qty");
+            if (response.id === 540541603618) {
+                dispensePad(size, qty, 'rfid', response.id)
+            }
+            console.log("RFID Text:", response.rfid_text);
+        }
+    };
+    xhr.send();
+}
+
+function QRPay() {
+    var size = getUrlParameter("size");
+    var qty = getUrlParameter("qty");
+    setTimeout(dispensePad, 4000, size, qty, 'payment', 'dummy')
+}
+
+function getUrlParameter(name) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(name)
+}
+
+function dispensePad(size, qty, type, id) {
     var xhr = new XMLHttpRequest();
 
     // API endpoint URL
-    const apiUrl = 'https://hygieia.life/TEST/padVending/inventory.php?size=' + size + '&qty=' + qty + '&payment=rfid_3694';
+    const apiUrl = 'https://hygieia.life/TEST/padVending/inventory.php?size=' + size + '&qty=' + qty + '&payment=' + type + '_' + id;
 
     fetch(apiUrl, {
         method: 'GET',
@@ -71,7 +111,7 @@ function choosePay() {
                 }
                 xhr.send();
 
-                location.reload();
+                window.location.href = './';
             } else {
                 console.error('API call failed:', response.status, response.statusText);
             }
